62786c6847f92f5a88f118ae6bfec54fbac24f33
NikolaRaicevic <nikolar047@gmail.com>
2024-04-11 20:19:50 -0700

    Added xArm-CPLUS-SDK

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ERL_Robot_Manipulation
+
+## Setup
+To install external repositories run: ./external_git_repositories.sh
--- external_git_repositories.sh
+++ external_git_repositories.sh
@@ -16,11 +16,24 @@ cd ../
 rosdep update
 rosdep install --from-paths . --ignore-src --rosdistro $ROS_DISTRO -y
 
+# 2) xArm-CPLUS-SDK
+git clone https://github.com/xArm-Developer/xArm-CPLUS-SDK.git
+cd ./xArm-CPLUS-SDK/
+make xarm
+make test
+make test-0002-get_property # build example/test-0002-get_property.cc
+make clean
+make # make xarm && make test
+# ./build/example/0002-get_property 192.168.1.221
+
+# 3) franka_ros2
+git clone https://github.com/frankaemika/franka_ros2.git 
+
+# 4) 
 
 
 
 
-
-##############
+##############################################################
 
 echo "Script execution completed: external_git_repositories"
